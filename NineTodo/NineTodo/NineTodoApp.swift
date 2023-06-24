7fef0b506f8bc470c33e2e7dc136c3e879be2919
doyeonjeong <debby_@kakao.com>
2023-06-24 14:18:28 +0900

    init

---
+++ NineTodo/NineTodo/NineTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NineTodoApp.swift
+//  NineTodo
+//
+//  Created by Doyeon on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NineTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
